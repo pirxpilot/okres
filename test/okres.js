9eb22906306f73fd63f46e6346e1b80c76c18987
Damian Krzeminski <pirxpilot@code42day.com>
2017-02-07 09:28:47 -0800

    add basic English formating

--- test/okres.js
+++ test/okres.js
@@ -2,6 +2,14 @@ var okres = require('../');
 
 describe('okres', function () {
   it('must format English durations', function () {
-    okres({ hours: 10, minute: 5 }).should.be.eql('10 hours 5 minutes');
+    okres({ hour: 10, minute: 5 }).should.be.eql('10 hours 5 minutes');
+  });
+
+  it('must skip zeros by default', function () {
+    okres({ day: 1, hour: 0, minute: 5 }).should.be.eql('a day 5 minutes');
+  });
+
+  it('must show zeros when required', function () {
+    okres({ day: 1, hour: 0, minute: 5 }, { showZero: true }).should.be.eql('a day 0 hours 5 minutes');
   });
 });

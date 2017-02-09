82788a2a1522e7e7eb45abb4d5114a28a5f8fd65
Damian Krzeminski <pirxpilot@code42day.com>
2017-02-08 16:42:14 -0800

    improve locale collator
    match de-de -> de etc.

---
+++ test/locale.js
@@ -0,0 +1,27 @@
+var locales = require('../lib/locales');
+
+require('../locale/pl');
+
+describe('locales', function () {
+  it('should select english by default', function () {
+    locales().should.have.property('relativeTime')
+      .with.property('s', 'a few seconds');
+    locales('en').should.be.exactly(locales());
+  });
+
+  it('should select english for unknown', function () {
+    locales('##').should.have.property('relativeTime')
+      .with.property('s', 'a few seconds');
+  });
+
+  it('should select locale when exact match found', function () {
+    locales('pl').should.have.property('relativeTime')
+      .with.property('s', 'kilka sekund');
+  });
+
+  it('should discard coutry when not found', function () {
+    locales('pl-pl').should.have.property('relativeTime')
+      .with.property('s', 'kilka sekund');
+  });
+});
+

1bda073f9975cde6ae6eb00937e1cdf51ca0dd79
Damian Krzeminski <pirxpilot@furkot.com>
2024-04-02 17:35:18 +0200

    replace mocha with node:test

--- test/okres.js
+++ test/okres.js
@@ -1,3 +1,4 @@
+const { describe, it, before } = require('node:test');
 const okres = require('../');
 
 require('../locale');
@@ -25,78 +26,86 @@ describe('okres en', function () {
 });
 
 describe('okres pl', function () {
+  let _okres;
+
   before(function() {
-    this.okres = okres('pl');
+    _okres = okres('pl');
   });
 
   it('should format durations', function () {
-    this.okres({ hour: 10, minute: 5 }).should.be.eql('10 godzin 5 minut');
+    _okres({ hour: 10, minute: 5 }).should.be.eql('10 godzin 5 minut');
   });
 
   it('should skip zeros by default', function () {
-    this.okres({ day: 1, hour: 0, minute: 5 }).should.be.eql('1 dzień 5 minut');
+    _okres({ day: 1, hour: 0, minute: 5 }).should.be.eql('1 dzień 5 minut');
   });
 
   it('should show zeros when required', function () {
-    this.okres({ day: 1, hour: 0, minute: 5 }, { showZero: true }).should.be.eql('1 dzień 0 godzin 5 minut');
+    _okres({ day: 1, hour: 0, minute: 5 }, { showZero: true }).should.be.eql('1 dzień 0 godzin 5 minut');
   });
 
   it('should format future', function () {
-    this.okres({ hour: 10, minute: 5 }, { future: true }).should.be.eql('za 10 godzin 5 minut');
+    _okres({ hour: 10, minute: 5 }, { future: true }).should.be.eql('za 10 godzin 5 minut');
   });
 
   it('should format past', function () {
-    this.okres({ hour: 10, minute: 5 }, { past: true }).should.be.eql('10 godzin 5 minut temu');
+    _okres({ hour: 10, minute: 5 }, { past: true }).should.be.eql('10 godzin 5 minut temu');
   });
 
   it('should format singular forms properly', function () {
-    this.okres({ hour: 1, minute: 5 }).should.be.eql('godzina 5 minut');
-    this.okres({ hour: 1, minute: 5 }, { future: true }).should.be.eql('za godzinę 5 minut');
-    this.okres({ hour: 1, minute: 5 }, { past: true }).should.be.eql('godzinę 5 minut temu');
+    _okres({ hour: 1, minute: 5 }).should.be.eql('godzina 5 minut');
+    _okres({ hour: 1, minute: 5 }, { future: true }).should.be.eql('za godzinę 5 minut');
+    _okres({ hour: 1, minute: 5 }, { past: true }).should.be.eql('godzinę 5 minut temu');
   });
 });
 
 
 describe('okres ro', function () {
+  let _okres;
+
   before(function() {
-    this.okres = okres('ro');
+    _okres = okres('ro');
   });
 
   it('should format durations', function () {
-    this.okres({ hour: 10, minute: 5 }).should.be.eql('10 ore 5 minute');
+    _okres({ hour: 10, minute: 5 }).should.be.eql('10 ore 5 minute');
   });
 
   it('should format future', function () {
-    this.okres({ hour: 10, minute: 5 }, { future: true }).should.be.eql('peste 10 ore 5 minute');
+    _okres({ hour: 10, minute: 5 }, { future: true }).should.be.eql('peste 10 ore 5 minute');
   });
 
   it('should format past', function () {
-    this.okres({ hour: 10, minute: 5 }, { past: true }).should.be.eql('10 ore 5 minute în urmă');
+    _okres({ hour: 10, minute: 5 }, { past: true }).should.be.eql('10 ore 5 minute în urmă');
   });
 
   it('should format singular forms properly', function () {
-    this.okres({ hour: 1, minute: 5 }).should.be.eql('o oră 5 minute');
-    this.okres({ hour: 1, minute: 5 }, { future: true }).should.be.eql('peste o oră 5 minute');
-    this.okres({ hour: 1, minute: 5 }, { past: true }).should.be.eql('o oră 5 minute în urmă');
+    _okres({ hour: 1, minute: 5 }).should.be.eql('o oră 5 minute');
+    _okres({ hour: 1, minute: 5 }, { future: true }).should.be.eql('peste o oră 5 minute');
+    _okres({ hour: 1, minute: 5 }, { past: true }).should.be.eql('o oră 5 minute în urmă');
   });
 });
 
 describe('okres bn', function () {
+  let _okres;
+
   before(function() {
-    this.okres = okres('bn');
+    _okres = okres('bn');
   });
 
   it('should format durations and use postformat', function () {
-    this.okres({ hour: 10, minute: 5 }).should.be.eql('১০ ঘন্টা ৫ মিনিট');
+    _okres({ hour: 10, minute: 5 }).should.be.eql('১০ ঘন্টা ৫ মিনিট');
   });
 });
 
 describe('okres ar', function () {
+  let _okres;
+
   before(function() {
-    this.okres = okres('ar');
+    _okres = okres('ar');
   });
 
   it('should format durations and use postformat', function () {
-    this.okres({ hour: 10, minute: 5 }, { future: true }).should.be.eql('بعد ١٠ ساعات ٥ دقائق');
+    _okres({ hour: 10, minute: 5 }, { future: true }).should.be.eql('بعد ١٠ ساعات ٥ دقائق');
   });
 });
